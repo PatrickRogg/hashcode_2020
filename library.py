062201bd93ff1f42c22c83fec182a0821d6aca64
emirbasal <basal.emir@gmail.com>
2020-02-20 19:11:17 +0100

    Added library and book class

---
+++ library.py
@@ -0,0 +1,8 @@
+
+class Library:
+    def __init__(self, id, books_set,  signup_time, number_books_per_day):
+        self.id = id
+        self.books_set = books_set
+        self.signup_time = signup_time
+        self.number_books_per_day = number_books_per_day
+        self.signup_process = signup_process
